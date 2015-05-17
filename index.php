c4e3a7d6be0f90852b25d15caf8ace1b1d9ced5e
Bhargava Srinarasi <bhargavabhat@gmail.com>
2015-05-17 12:35:01 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo    '<ul>
+            <li> <a href="addIngredient.php"> Add Ingredient</a> </li>
+            <li> <a href="addRecipe.php"> Add Recipe</a> </li>
+            <li> <a href="showRecipes.php"> Show Recipes</a> </li>
+            <li> <a href="search.php"> Search</a> </li>
+        </ul>';
+?>
+
+</body>
+</html>
